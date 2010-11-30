019c708bf145a8bb84f999bcca67d35f32c3f9d6
Frank Duckhorn <git@bitboulder.net>
2010-11-30 07:56:37 +0000

    bugfix in DE_NEG
    git-svn-id: file:///home/svn/slideshowgl/trunk/slideshowgl/slideshowgl-2.0.0@137 bbf4d130-548c-4f79-ac20-24e14d931773

--- src/dpl.h
+++ src/dpl.h
@@ -28,24 +28,24 @@ struct icol {
 enum effrefresh { EFFREF_NO=0x0, EFFREF_IMG=0x1, EFFREF_ALL=0x2, EFFREF_FIT=0x4 };
 
 enum dplev {
-	DE_MOVE    = 0x001,
-	DE_RIGHT   = 0x002,
-	DE_LEFT    = 0x004,
-	DE_UP      = 0x008,
-	DE_DOWN    = 0x010,
-	DE_ZOOMIN  = 0x020,
-	DE_ZOOMOUT = 0x040,
-	DE_ROT1    = 0x080,
-	DE_ROT2    = 0x100,
-	DE_PLAY    = 0x200,
-	DE_KEY     = 0x400,
-	DE_STAT    = 0x800,
+	DE_MOVE    = 0x0001,
+	DE_RIGHT   = 0x0002,
+	DE_LEFT    = 0x0004,
+	DE_UP      = 0x0008,
+	DE_DOWN    = 0x0010,
+	DE_ZOOMIN  = 0x0020,
+	DE_ZOOMOUT = 0x0040,
+	DE_ROT1    = 0x0080,
+	DE_ROT2    = 0x0100,
+	DE_PLAY    = 0x0200,
+	DE_KEY     = 0x0400,
+	DE_STAT    = 0x0800,
 };
 #define DE_HOR		(DE_RIGHT|DE_LEFT)
 #define DE_VER  	(DE_UP|DE_DOWN)
 #define DE_ZOOM		(DE_ZOOMIN|DE_ZOOMOUT)
 #define DE_DIR(ev)	(((ev)&(DE_RIGHT|DE_UP|DE_ZOOMIN|DE_ROT1))?1:(((ev)&(DE_LEFT|DE_DOWN|DE_ZOOMOUT|DE_ROT2))?-1:0))
-#define DE_NEG(ev)	(DE_DIR(ev)>0?(ev<<1):(DE_DIR(ev)<0?(ev>>1):0))
+#define DE_NEG(ev)	(DE_DIR(ev)>0?((ev)<<1):(DE_DIR(ev)<0?((ev)>>1):(ev)))
 
 void effrefresh(enum effrefresh val);
 int dplgetimgi();
