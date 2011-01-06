83d7f42b112d4a169cf3810f7d2308368e9edd6f
Frank Duckhorn <git@bitboulder.net>
2011-01-06 16:58:12 +0000

    blending is no object of fragment shader
    git-svn-id: file:///home/svn/slideshowgl/trunk/slideshowgl/slideshowgl-2.0.0@234 bbf4d130-548c-4f79-ac20-24e14d931773

--- data/fs.c
+++ data/fs.c
@@ -2,6 +2,7 @@ uniform sampler2D tex;
 void main(){
 	vec4 color;
 	
+	// texture enabled?
 	if(gl_SecondaryColor[0]>0){
 		color = texture2D(tex,gl_TexCoord[0].st);
 
@@ -9,11 +10,12 @@ void main(){
 		if(gl_Color.x!=0.5) color = pow(color,-log2(gl_Color.x));
 		if(gl_Color.y!=0.5) color = (0.5-color)*log2(1-gl_Color.y) + 0.5;
 		if(gl_Color.z!=0.5) color = color + (gl_Color.z - 0.5)*2;
+
+		color.a = gl_Color.a;
 	}else{
 		color = gl_Color;
 	}
 
-	// blend
-	gl_FragColor.a = color.a*gl_Color.a + gl_FragColor.a*(1-gl_Color.a);
-	gl_FragColor.rgb = color.rgb;
+	// output
+	gl_FragColor = color;
 }
